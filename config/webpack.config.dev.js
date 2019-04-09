fa656e046918e47ded9f5db5d75b9e3d4f30047a
Eric Henderson <henderea@gmail.com>
2019-04-09 09:26:11 -0400

    switch to workbox-webpack-plugin

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -2,9 +2,8 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
-const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const {GenerateSW} = require('workbox-webpack-plugin');
 const getClientEnvironment = require('./env');
 const paths = require('./paths');
 
@@ -124,32 +123,13 @@ module.exports = {
             fileName: 'asset-manifest.json',
             publicPath: publicPath
         }),
-        new SWPrecacheWebpackPlugin({
-            // By default, a cache-busting query parameter is appended to requests
-            // used to populate the caches, to ensure the responses are fresh.
-            // If a URL is already hashed by Webpack, then there is no concern
-            // about it being stale, and the cache-busting can be skipped.
-            dontCacheBustUrlsMatching: /\.\w{8}\./,
-            filename: 'service-worker.js',
-            logger(message) {
-                if(message.indexOf('Total precache size is') === 0) {
-                    // This message occurs for every build and is a bit too noisy.
-                    return;
-                }
-                if(message.indexOf('Skipping static resource') === 0) {
-                    // This message obscures real errors so we ignore it.
-                    // https://github.com/facebook/create-react-app/issues/2612
-                    return;
-                }
-                console.log(message);
-            },
-            minify: true,
+        new GenerateSW({
             // Don't precache sourcemaps (they're large) and build asset manifest:
-            staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
+            exclude: [/\.map$/, /asset-manifest\.json$/],
             // `navigateFallback` and `navigateFallbackWhitelist` are disabled by default; see
             // https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md#service-worker-considerations
             navigateFallback: publicUrl + '/index.html',
-            navigateFallbackWhitelist: [/^(?!\/_).*/],
+            navigateFallbackBlacklist: [/^\/_/],
         }),
     ],
     performance: {
