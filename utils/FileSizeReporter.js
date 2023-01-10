a1c71071811521d428576fc94175d2569fc4ed47
Eric Henderson <henderea@gmail.com>
2023-01-10 12:38:42 -0500

    fix an issue

--- utils/FileSizeReporter.js
+++ utils/FileSizeReporter.js
@@ -10,7 +10,7 @@
 import fs from 'fs';
 import path from 'path';
 import chalk from 'chalk';
-import filesize from 'filesize';
+import { filesize } from 'filesize';
 import recursive from 'recursive-readdir';
 import stripAnsi from 'strip-ansi';
 import { gzipSizeSync } from 'gzip-size';
